e64083a9721eb5994b6659231217d09647fcf99d
ry4n-s <132171741+ry4n-s@users.noreply.github.com>
2023-05-11 18:38:44 -0400

    Add files via upload

---
+++ machinetranslation/translator.py
@@ -0,0 +1,29 @@
+'''All imports'''
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(english_text):
+    '''Translates English to French'''
+    french_text = language_translator.translate(english_text, model_id='en-fr').get_result()
+    return french_text.get('translations')[0].get('translation')
+
+
+def frenchToEnglish(french_text):
+    '''Translates French to English'''
+    english_text = language_translator.translate(french_text, model_id='fr-en').get_result()
+    return english_text.get('translations')[0].get('translation')
